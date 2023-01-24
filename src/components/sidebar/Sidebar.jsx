7b3454abcb0ca7a368243d85e0a049346111e24b
OutcastR97 <sadman.rashiq.dbz@gmail.com>
2023-01-24 18:16:49 +0600

    sidebar-added

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,94 @@
+import { AttachMoney, BarChart, ChatOutlined, EmailOutlined, FeedbackOutlined, LineStyle, PersonOutline, Report, Storefront, TrendingUp, WorkOutline } from '@mui/icons-material'
+import TimelineIcon from '@mui/icons-material/Timeline';
+import './Sidebar.css'
+
+const Sidebar = () => {
+    return (
+        <div className='sidebar'>
+            <div className="sidebarWrapper">
+                <div className="sidebarMenu">
+                    <h3 className="sidebarTitle">
+                        Dashboard
+                    </h3>
+                    <ul className="sidebarList">
+                        <li className="sidebarListItem active">
+                            <LineStyle className='sidebarIcon' />
+                            Home
+                        </li>
+                        <li className="sidebarListItem">
+                            <TimelineIcon className='sidebarIcon' />
+                            Analytics
+                        </li>
+                        <li className="sidebarListItem">
+                            <TrendingUp className='sidebarIcon' />
+                            Sales
+                        </li>
+                    </ul>
+                </div>
+                <div className="sidebarMenu">
+                    <h3 className="sidebarTitle">
+                        Quick Menu
+                    </h3>
+                    <ul className="sidebarList">
+                        <li className="sidebarListItem active">
+                            <PersonOutline className='sidebarIcon' />
+                            Users
+                        </li>
+                        <li className="sidebarListItem">
+                            <Storefront className='sidebarIcon' />
+                            Products
+                        </li>
+                        <li className="sidebarListItem">
+                            <AttachMoney className='sidebarIcon' />
+                            Transactions
+                        </li>
+                        <li className="sidebarListItem">
+                            <BarChart className='sidebarIcon' />
+                            Reports
+                        </li>
+                    </ul>
+                </div>
+                <div className="sidebarMenu">
+                    <h3 className="sidebarTitle">
+                        Notifications
+                    </h3>
+                    <ul className="sidebarList">
+                        <li className="sidebarListItem active">
+                            <EmailOutlined className='sidebarIcon' />
+                            Mail
+                        </li>
+                        <li className="sidebarListItem">
+                            <FeedbackOutlined className='sidebarIcon' />
+                            Feedback
+                        </li>
+                        <li className="sidebarListItem">
+                            <ChatOutlined className='sidebarIcon' />
+                            Messages
+                        </li>
+                    </ul>
+                </div>
+                <div className="sidebarMenu">
+                    <h3 className="sidebarTitle">
+                        Staff
+                    </h3>
+                    <ul className="sidebarList">
+                        <li className="sidebarListItem active">
+                            <WorkOutline className='sidebarIcon' />
+                            Manage
+                        </li>
+                        <li className="sidebarListItem">
+                            <TimelineIcon className='sidebarIcon' />
+                            Analytics
+                        </li>
+                        <li className="sidebarListItem">
+                            <Report className='sidebarIcon' />
+                            Reports
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
