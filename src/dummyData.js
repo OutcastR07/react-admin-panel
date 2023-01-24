7883f218143470b23b85cd8e62bccd4b82cc1f72
OutcastR97 <sadman.rashiq.dbz@gmail.com>
2023-01-24 21:55:07 +0600

    react-chart-used

---
+++ src/dummyData.js
@@ -0,0 +1,46 @@
+export const userData = [
+    {
+        name: 'Jan',
+        "Active User": 4000,
+    },
+    {
+        name: 'Feb',
+        "Active User": 3000,
+    },
+    {
+        name: 'Mar',
+        "Active User": 2000,
+    },
+    {
+        name: 'May',
+        "Active User": 2780,
+    },
+    {
+        name: 'Jun',
+        "Active User": 1890,
+    },
+    {
+        name: 'Jul',
+        "Active User": 2390,
+    },
+    {
+        name: 'Aug',
+        "Active User": 3490,
+    },
+    {
+        name: 'Sep',
+        "Active User": 4120,
+    },
+    {
+        name: 'Oct',
+        "Active User": 3161,
+    },
+    {
+        name: 'Nov',
+        "Active User": 3370,
+    },
+    {
+        name: 'Dec',
+        "Active User": 2901,
+    },
+];
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,13 @@
+import Charts from '../../components/charts/Charts'
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo'
 import './Home.css'
+import { userData } from '../../dummyData'
 
 const Home = () => {
     return (
         <div className='home'>
             <FeaturedInfo />
+            <Charts data={userData} title="User Analytics" grid dataKey="Active User" />
         </div>
     )
 }
