01b22d4a3030c5adebb104ec9815da922314dd6b
OutcastR97 <sadman.rashiq.dbz@gmail.com>
2023-01-25 19:34:38 +0600

    user-info-components

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,18 @@ import Charts from '../../components/charts/Charts'
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo'
 import './Home.css'
 import { userData } from '../../dummyData'
+import WidgetLg from '../../components/widgetLg/WidgetLg'
+import WidgetSm from '../../components/widgetSm/WidgetSm'
 
 const Home = () => {
     return (
         <div className='home'>
             <FeaturedInfo />
             <Charts data={userData} title="User Analytics" grid dataKey="Active User" />
+            <div className="homeWidgets">
+                <WidgetSm />
+                <WidgetLg />
+            </div>
         </div>
     )
 }
