e5037fc293e162c348ceb9efbb5b6829ba264b20
OutcastR97 <sadman.rashiq.dbz@gmail.com>
2023-01-26 01:07:48 +0600

    data-table-added

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 import './App.css'
 import Home from "./pages/home/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import UserList from "./pages/userList/UserList";
 
 function App() {
   return (
-    <div>
+    <Router>
       <Topbar />
       <div className="container">
         <Sidebar />
-        <Home />
+        <Routes>
+          <Route path="/" exact={true} element={<Home />} />
+          <Route path="/users" element={<UserList />} />
+        </Routes>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- src/dummyData.js
+++ src/dummyData.js
@@ -44,3 +44,76 @@ export const userData = [
         "Active User": 2901,
     },
 ];
+
+export const userRows = [
+    {
+        id: 1, username: 'Jon Snow',
+        avatar: 'https://th.bing.com/th/id/OIP.Tw9NCdBONF4I4RyJzbC08wHaKW?pid=ImgDet&rs=1',
+        email: 'jon@gmail.com',
+        status: 'active',
+        transaction: '$120.00',
+    },
+    {
+        id: 2, username: 'Jon Snow',
+        avatar: 'https://th.bing.com/th/id/OIP.Tw9NCdBONF4I4RyJzbC08wHaKW?pid=ImgDet&rs=1',
+        email: 'jon@gmail.com',
+        status: 'active',
+        transaction: '$120.00',
+    },
+    {
+        id: 3, username: 'Jon Snow',
+        avatar: 'https://th.bing.com/th/id/OIP.Tw9NCdBONF4I4RyJzbC08wHaKW?pid=ImgDet&rs=1',
+        email: 'jon@gmail.com',
+        status: 'active',
+        transaction: '$120.00',
+    },
+    {
+        id: 4, username: 'Jon Snow',
+        avatar: 'https://th.bing.com/th/id/OIP.Tw9NCdBONF4I4RyJzbC08wHaKW?pid=ImgDet&rs=1',
+        email: 'jon@gmail.com',
+        status: 'active',
+        transaction: '$120.00',
+    },
+    {
+        id: 5, username: 'Jon Snow',
+        avatar: 'https://th.bing.com/th/id/OIP.Tw9NCdBONF4I4RyJzbC08wHaKW?pid=ImgDet&rs=1',
+        email: 'jon@gmail.com',
+        status: 'active',
+        transaction: '$120.00',
+    },
+    {
+        id: 6, username: 'Jon Snow',
+        avatar: 'https://th.bing.com/th/id/OIP.Tw9NCdBONF4I4RyJzbC08wHaKW?pid=ImgDet&rs=1',
+        email: 'jon@gmail.com',
+        status: 'active',
+        transaction: '$120.00',
+    },
+    {
+        id: 7, username: 'Jon Snow',
+        avatar: 'https://th.bing.com/th/id/OIP.Tw9NCdBONF4I4RyJzbC08wHaKW?pid=ImgDet&rs=1',
+        email: 'jon@gmail.com',
+        status: 'active',
+        transaction: '$120.00',
+    },
+    {
+        id: 8, username: 'Jon Snow',
+        avatar: 'https://th.bing.com/th/id/OIP.Tw9NCdBONF4I4RyJzbC08wHaKW?pid=ImgDet&rs=1',
+        email: 'jon@gmail.com',
+        status: 'active',
+        transaction: '$120.00',
+    },
+    {
+        id: 9, username: 'Jon Snow',
+        avatar: 'https://th.bing.com/th/id/OIP.Tw9NCdBONF4I4RyJzbC08wHaKW?pid=ImgDet&rs=1',
+        email: 'jon@gmail.com',
+        status: 'active',
+        transaction: '$120.00',
+    },
+    {
+        id: 10, username: 'Jon Snow',
+        avatar: 'https://th.bing.com/th/id/OIP.Tw9NCdBONF4I4RyJzbC08wHaKW?pid=ImgDet&rs=1',
+        email: 'jon@gmail.com',
+        status: 'active',
+        transaction: '$120.00',
+    },
+];
